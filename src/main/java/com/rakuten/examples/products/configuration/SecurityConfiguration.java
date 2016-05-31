806913a87e239d3cd044e0c4cf65e70021bd148d
paravena <paravena74@gmail.com>
2016-05-31 14:00:00 -0300

    Adding security configuration http basic

--- src/main/java/com/rakuten/examples/products/configuration/SecurityConfiguration.java
+++ src/main/java/com/rakuten/examples/products/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.rakuten.examples.products.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,12 +9,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN, USER").and()
+                .withUser("user").password("user").roles("USER");
+    }
+
+    @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-                .authorizeRequests().antMatchers("/product/**").permitAll();
+        httpSecurity.authorizeRequests()
+                .antMatchers("/product/*")
+                .access("hasRole('USER')")
+                .and().httpBasic()
+                .realmName("Products");
 
         httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();
+        httpSecurity
+                .headers()
+                .frameOptions()
+                .disable();
     }
 
 }
